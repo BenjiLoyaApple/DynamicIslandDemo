c1a1af9fd5c7dd4df18b3b236ac78516e1a0426d
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-10-29 17:07:55 +0500

    Initial Commit

---
+++ DynamicIslandDemo/DynamicIslandDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandDemoApp.swift
+//  DynamicIslandDemo
+//
+//  Created by Benji Loya on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
